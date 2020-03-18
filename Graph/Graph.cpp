bf107f1bea614f8b8b77d965c21e67f310a002dd
jakub530 <jakub530@gmail.com>
2020-03-18 09:27:04 +0000

    Working djikstra slow mechanism to find smallest node

--- Graph/Graph.cpp
+++ Graph/Graph.cpp
@@ -19,7 +19,10 @@ public:
 
     Edge()
     {
-
+        source=-1;
+        dest = -1;
+        cost = -1;
+        bi =0;
     }
 
     Edge(int _source, int _dest, int _cost,bool _bi)
@@ -46,22 +49,19 @@ public:
         std::cout << "Biderectional: " << bi << "\n";
         std::cout << "\n";
     }
-
-
 };
 
 
-bool cost_comp(const Edge& e1, const Edge& e2)
-{
-    return e1.cost < e2.cost;
-}
+
+
+
 
 class Node
 {
 public:
     std::vector<Edge> connections;
     int group = -1;
-    int index;
+    int index = -1;
     void add_connection(Edge connection)
     {
         connections.push_back(connection);
@@ -82,6 +82,31 @@ public:
     }
 };
 
+class Node_Djikstra : public Node
+{
+public:
+    double dist;
+    int prev;
+    bool active;
+
+    Node_Djikstra(Node node)
+    {
+        connections = node.connections;
+        index = node.index;
+        dist = std::numeric_limits<int>::max();
+        prev = -1;
+        active = true;
+    }
+
+    Node_Djikstra()
+    {
+        dist = -1;
+        prev = -1;
+        active = false;
+    }
+
+};
+
 class Group
 {
 public:
@@ -96,10 +121,45 @@ public:
 
     Group()
     {
-
+        group_index = -1;
     }
 };
 
+bool cost_comp(const Edge& e1, const Edge& e2)
+{
+    return e1.cost < e2.cost;
+}
+
+void edge_transform(std::vector<std::vector<int>>& data)
+{
+    for (size_t i = 0;i<data.size();i++)
+    {
+        data[i][0]--;
+        data[i][1]--;
+    }
+}
+
+bool dist_comp(const Node_Djikstra& n1, const Node_Djikstra& n2)
+{
+    return n1.dist > n2.dist;
+}
+
+std::vector<std::vector<int>> gather_data(std::vector<int> source, std::vector<int> dest, std::vector<int> cost = {})
+{
+    std::vector<std::vector<int>> data;
+    for (size_t i = 0; i < source.size(); i++)
+    {
+        std::vector<int> tmp_vector;
+        tmp_vector.push_back(source[i]);
+        tmp_vector.push_back(dest[i]);
+        if (cost.size() != 0)
+        {
+            tmp_vector.push_back(cost[i]);
+        }
+        data.push_back(tmp_vector);
+    }
+    return data;
+}
 
 class Graph
 {
@@ -111,7 +171,7 @@ public:
 
     Graph()
     {
-
+        sum_cost = 0;
     }
 
     void create_nodes(int size, bool assign_group)
@@ -241,7 +301,7 @@ public:
 
     void assimilate_group(int  min_group_index, int max_group_index)
     {
-        for (int i = 0; i < Groups[max_group_index].nodes.size(); i++)
+        for (size_t i = 0; i < Groups[max_group_index].nodes.size(); i++)
         {
             Groups[min_group_index].nodes.push_back(Groups[max_group_index].nodes[i]);
             Nodes[Groups[max_group_index].nodes[i]].group = Groups[min_group_index].group_index;
@@ -281,12 +341,93 @@ public:
         //std::cout << "Edge cost sum is " << sum << "\n"; 
         return min_graph;
     }
+
+     
+    int find_smallest_cost(const std::vector<Node_Djikstra> &d_nodes)
+    {
+        double smallest = std::numeric_limits<int>::max();
+        int min_index = 0;
+        for (auto node : d_nodes)
+        {
+            if (node.active == true)
+            {
+                if (node.dist <= smallest)
+                {
+                    smallest = node.dist;
+                    min_index = node.index;
+                }
+            }
+        }
+        return min_index;
+    }
+
+    double dijkstra(int source, int dest)
+    {
+        std::vector<Node_Djikstra> d_nodes(Nodes.size());
+        std::vector<int> p_nodes;
+        for (size_t i = 0; i < d_nodes.size(); i++)
+        {
+            d_nodes[i] = Node_Djikstra(Nodes[i]);
+        }
+        int p_node_ind = 0;
+        d_nodes[source].dist = 0;
+        p_nodes.push_back(source);
+        Node_Djikstra tmp_node;
+        int min_node = source;
+        int inactive_nodes = 0;
+        bool flag = false;
+
+        while (true)
+        {
+            d_nodes[min_node].active = false;
+            inactive_nodes++;
+            Node_Djikstra u_node = d_nodes[min_node];
+            
+
+            if (u_node.index == dest)
+            {
+                tmp_node = u_node;
+                flag = true;
+                break;
+            }
+
+            for (auto connection : u_node.connections)
+            {
+                double alt = std::max(u_node.dist,double(connection.cost));
+                if (alt < d_nodes[connection.dest].dist)
+                {
+                    d_nodes[connection.dest].dist = alt;
+                    d_nodes[connection.dest].prev = u_node.index;
+
+                }
+
+            }
+            min_node = find_smallest_cost(d_nodes);
+
+            if (inactive_nodes == Nodes.size())
+            {
+                break;
+            }
+        }
+ 
+        if (flag)
+        {
+            return tmp_node.dist;
+        }
+        else
+        {
+            return -1;
+        }
+
+    }
+
+
 };
 
 int main()
 {
     std::vector<std::vector<int>> vect{ { 0, 1 ,3 },
-                                        { 1, 2 , 2}, 
+                                        { 1, 2 , 4}, 
                                         { 2, 3 , 6}, 
                                         { 1, 3 , 4}, 
                                         { 2, 5 , 2}, 
@@ -297,12 +438,8 @@ int main()
     Graph min_graph = New_Graph.find_min_graph();
     min_graph.print_edges();
     std::cout << "Edge cost sum is " << min_graph.sum_cost << "\n";
-    //New_Graph.sort_edges();
-    //New_Graph.print_edges();
+    std::cout << New_Graph.dijkstra(0, 3) << "\n";
 
-    //New_Graph.create_groups();
-    //New_Graph.print_groups();
-    //New_Graph.print_nodes();
     std::vector<int> lol;
     
 }
